eb8d8d836584121c7b0c0685fbe9bb3021213069
Cédric Esteba Serra <cedricbri@gmail.com>
2020-10-05 17:38:09 +0200

    [ ADD TESTS ] Added 4 tests OK
    Sense tocar la classe original, crear un test nou per cada mètode i buscar un conjunt de paràmetres d'entrada/resultat esperat que faci passar aquests 4 tests nous.

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ExerciseIntroTestingTest {
 
+    // Old tests
+
     @Test
     public void testFindLast(){
         final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5}, 2);
@@ -27,6 +29,32 @@ public class ExerciseIntroTestingTest {
     @Test
     public void testOddOrPos(){
         final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
+        assertEquals(result, 3);
+    }
+
+    // New tests
+
+    @Test
+    public void testFindLastNew(){
+        final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5, 3, 4}, 3);
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testLastZeroNew(){
+        final int result = ExerciseIntroTesting.lastZero(new int []{1, 3, 0, 5});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testCountPositiveNew(){
+        final int result = ExerciseIntroTesting.countPositive(new int []{-4, 2, 1, 2});
+        assertEquals(result, 3);
+    }
+
+    @Test
+    public void testOddOrPosNew(){
+        final int result = ExerciseIntroTesting.oddOrPos(new int []{3, -2, 2, 1, -4});
         assertEquals(result, 3);
     }
 }
